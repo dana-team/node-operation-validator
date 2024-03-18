cae5352c78da5abe9e606bdac362c8163cb965a6
mzeevi <meytar80@gmail.com>
2024-03-18 20:20:35 +0000

    initial commit
    Signed-off-by: mzeevi <meytar80@gmail.com>

---
+++ internal/webhook/webhook.go
@@ -0,0 +1,144 @@
+package webhook
+
+import (
+	"context"
+	"fmt"
+	admissionv1 "k8s.io/api/admission/v1"
+	"net/http"
+	"os"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"strings"
+
+	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+// NodeValidator is the struct used to validate the nodes
+type NodeValidator struct {
+	Decoder *admission.Decoder
+	Client  client.Client
+}
+
+// Operation represents the type of operation being performed
+type Operation string
+
+const (
+	reasonAnnotation             = "node.dana.io/reason"
+	serviceAccountUser           = "system:serviceaccount:"
+	systemAdminUser              = "system:admin"
+	ForbiddenUsersEnv            = "forbiddenUsers"
+	Create             Operation = "create"
+	Delete             Operation = "delete"
+	Cordon             Operation = "cordon"
+	Uncordon           Operation = "uncordon"
+)
+
+// +kubebuilder:webhook:path=/validate-v1-node,mutating=false,failurePolicy=ignore,sideEffects=None,groups=core,resources=nodes,verbs=delete;create;update,versions=v1,name=nodeoperation.dana.io,admissionReviewVersions=v1
+
+func (n *NodeValidator) Handle(ctx context.Context, req admission.Request) admission.Response {
+	logger := log.FromContext(ctx).WithName("Node Webhook").WithValues("node", req.Name)
+
+	node := corev1.Node{}
+	oldNode := corev1.Node{}
+	user := req.UserInfo.Username
+
+	forbiddenUsers := strings.Split(os.Getenv(ForbiddenUsersEnv), ",")
+	forbiddenUsers = append(forbiddenUsers, systemAdminUser)
+
+	switch req.Operation {
+	case admissionv1.Delete:
+		if err := n.Decoder.DecodeRaw(req.OldObject, &node); err != nil {
+			return admission.Errored(http.StatusBadRequest, fmt.Errorf("failed to decode node %q", req.Name))
+		}
+		reasonMessage, doesReasonExist := node.Annotations[reasonAnnotation]
+		return userOnlyOperation(Delete, user, forbiddenUsers, reasonMessage, logger, true, doesReasonExist)
+
+	case admissionv1.Create:
+		if err := n.Decoder.DecodeRaw(req.Object, &node); err != nil {
+			return admission.Errored(http.StatusBadRequest, fmt.Errorf("failed to decode node %q", req.Name))
+		}
+		_, doesReasonExist := node.Annotations[reasonAnnotation]
+		return validateNoReason(doesReasonExist, logger, Create, user)
+
+	// The default case handles the update requests.
+	default:
+		if err := n.Decoder.DecodeRaw(req.OldObject, &node); err != nil {
+			return admission.Errored(http.StatusBadRequest, fmt.Errorf("failed to decode node %q", req.Name))
+		}
+		if err := n.Decoder.DecodeRaw(req.Object, &node); err != nil {
+			return admission.Errored(http.StatusBadRequest, fmt.Errorf("failed to decode node %q", req.Name))
+		}
+		reasonMessage, doesReasonExist := node.Annotations[reasonAnnotation]
+
+		switch {
+		case !oldNode.Spec.Unschedulable && node.Spec.Unschedulable:
+			return userOnlyOperation(Cordon, user, forbiddenUsers, reasonMessage, logger, true, doesReasonExist)
+
+		case oldNode.Spec.Unschedulable && !node.Spec.Unschedulable:
+			return userOnlyOperation(Uncordon, user, forbiddenUsers, reasonMessage, logger, false, doesReasonExist)
+
+		default:
+			return admission.Allowed("Node was updated")
+		}
+	}
+}
+
+// userOnlyOperation checks whether a given user is allowed to perform a specific operation on a node.
+// It returns an admission response indicating whether the operation is allowed or denied.
+func userOnlyOperation(operation Operation, user string, forbiddenUsers []string, reasonMessage string, log logr.Logger, isReasonRequired bool, doesReasonExist bool) admission.Response {
+	switch {
+	case isForbiddenUser(user, forbiddenUsers):
+		log.Info(fmt.Sprintf("%s node denied", operation), "DenialReason", "forbidden user", "User", user)
+		if doesReasonExist {
+			return admission.Denied(fmt.Sprintf("%q user is not allowed to %s a node. Please log in with a LDAP privileged user", user, operation))
+		} else {
+			return admission.Denied(fmt.Sprintf("%q user is not allowed to %s a node. Please log in with a LDAP privileged user. You must also add %q annotation", user, operation, reasonAnnotation))
+		}
+
+	case isServiceAccount(user):
+		log.Info(fmt.Sprintf("%s node approved", operation), "User", user, "ApprovalReason", "Service account is allowed to do any operation")
+		return admission.Allowed(fmt.Sprintf("Service account %q is allowed to do everything", user))
+
+	default:
+		if isReasonRequired {
+			if doesReasonExist {
+				log.Info(fmt.Sprintf("%s node approved", operation), "User", user, "Reason", reasonMessage)
+				return admission.Allowed(fmt.Sprintf("%s operation has been approved", operation))
+			} else {
+				log.Info(fmt.Sprintf("%s node denied", operation), "DenialReason", "reason annotation doesn't exist", "User", user)
+				return admission.Denied(fmt.Sprintf("You must add %q annotation", reasonAnnotation))
+			}
+		} else {
+			return validateNoReason(doesReasonExist, log, operation, user)
+		}
+	}
+}
+
+// validateNoReason checks if reason annotation exists when doing an operation.
+// If the reason exists, it denies the request. If it doesn't - the operation is approved and logged.
+func validateNoReason(doesReasonExist bool, log logr.Logger, operation Operation, user string) admission.Response {
+	if doesReasonExist {
+		log.Info(fmt.Sprintf("%s node denied", operation), "DenialReason", "reason annotation exists", "User", user)
+		return admission.Denied(fmt.Sprintf("Don't forget to remove the %q annotation from the node", reasonAnnotation))
+	} else {
+		log.Info(fmt.Sprintf("%s node approved", operation), "User", user)
+		return admission.Allowed("Operation approved")
+	}
+}
+
+// isServiceAccount returns true if the given user is a service account
+func isServiceAccount(user string) bool {
+	return strings.HasPrefix(user, serviceAccountUser)
+}
+
+// isForbiddenUser checks if the given user is in the list of forbidden users.
+func isForbiddenUser(userToCheck string, forbiddenUsers []string) bool {
+	for _, user := range forbiddenUsers {
+		if user == userToCheck {
+			return true
+		}
+	}
+	return false
+}
---
+++ internal/webhook/webhook_test.go
@@ -0,0 +1,160 @@
+package webhook
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+	"testing"
+
+	. "github.com/onsi/gomega"
+	admissionv1 "k8s.io/api/admission/v1"
+	v1 "k8s.io/api/authentication/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	testclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+const (
+	regularUserExample = "user"
+)
+
+func newScheme() *runtime.Scheme {
+	s := runtime.NewScheme()
+	_ = corev1.AddToScheme(s)
+	_ = scheme.AddToScheme(s)
+	return s
+}
+
+func newFakeClient() client.Client {
+	scm := newScheme()
+	return testclient.NewClientBuilder().WithScheme(scm).Build()
+}
+
+func TestNodeWebhook(t *testing.T) {
+	//webhookLog := ctrl.Log.WithName("Node webhook")
+	tests := []struct {
+		name      string
+		operation admissionv1.Operation
+		user      string
+		reason    string
+		allowed   bool
+	}{
+		{name: "CreateWithReason", operation: admissionv1.Create, user: regularUserExample, reason: "testing", allowed: false},
+		{name: "CreateWithoutReason", operation: admissionv1.Create, user: systemAdminUser, reason: "", allowed: true},
+		{name: "DeleteAsKubeadminWithReason", operation: admissionv1.Delete, user: systemAdminUser, reason: "testing", allowed: false},
+		{name: "DeleteAsUserWithoutReason", operation: admissionv1.Delete, user: regularUserExample, reason: "", allowed: false},
+		{name: "DeleteAsUserWithReason", operation: admissionv1.Delete, user: regularUserExample, reason: "testing", allowed: true},
+		{name: "CordonAsKubeadminWithReason", operation: "cordon", user: systemAdminUser, reason: "testing", allowed: false},
+		{name: "CordonAsUserWithoutReason", operation: "cordon", user: regularUserExample, reason: "", allowed: false},
+		{name: "CordonAsUserWithReason", operation: "cordon", user: regularUserExample, reason: "testing", allowed: true},
+		{name: "CordonAsServiceAccountWithoutReason", operation: "cordon", user: serviceAccountUser + "openshift-machine-config-operator:machine-config-daemon", reason: "", allowed: true},
+		{name: "UncordonAsKubeadminWithoutReason", operation: "uncordon", user: systemAdminUser, reason: "", allowed: false},
+		{name: "UncordonAsUserWithReason", operation: "uncordon", user: regularUserExample, reason: "testing", allowed: false},
+		{name: "UncordonAsUserWithoutReason", operation: "uncordon", user: regularUserExample, reason: "", allowed: true},
+		{name: "UncordonAsServiceAccountWithReason", operation: "uncordon", user: serviceAccountUser + "openshift-machine-config-operator:machine-config-daemon", reason: "testing", allowed: true},
+	}
+	fakeClient := newFakeClient()
+	ctx := context.Background()
+	g := NewWithT(t)
+	decoder := admission.NewDecoder(scheme.Scheme)
+	nv := NodeValidator{Decoder: decoder, Client: fakeClient}
+	err := os.Setenv(ForbiddenUsersEnv, systemAdminUser)
+	if err != nil {
+		print(err)
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			annotations := make(map[string]string)
+			if test.reason != "" {
+				annotations[reasonAnnotation] = test.reason
+			}
+			node := corev1.Node{
+				ObjectMeta: metav1.ObjectMeta{Name: test.name,
+					Annotations: annotations},
+			}
+			cordonedNode := corev1.Node{
+				ObjectMeta: metav1.ObjectMeta{Name: test.name,
+					Annotations: annotations},
+				Spec: corev1.NodeSpec{Unschedulable: true},
+			}
+			nodeObj, err := json.Marshal(node)
+			g.Expect(err).ShouldNot(HaveOccurred())
+
+			cordonedNodeObj, err := json.Marshal(cordonedNode)
+			g.Expect(err).ShouldNot(HaveOccurred())
+
+			// In case of create operation - tries to create a node and ensures the response is as expected.
+			if test.operation == admissionv1.Create {
+				createReq := admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{Name: test.name,
+					Operation: test.operation,
+					UserInfo:  v1.UserInfo{Username: test.user},
+					Kind:      metav1.GroupVersionKind{Kind: "Node", Group: "core", Version: "v1"},
+					Object:    runtime.RawExtension{Raw: nodeObj}}}
+				response := nv.Handle(ctx, createReq)
+				g.Expect(response.Allowed).Should(Equal(test.allowed))
+			}
+
+			// In case of delete operation - create an empty node, add relevant annotations to it and update,
+			// tries to delete the node with the given user and reason, and ensures the response is as expected.
+			if test.operation == admissionv1.Delete {
+				emptyNode := corev1.Node{
+					ObjectMeta: metav1.ObjectMeta{Name: test.name},
+				}
+				err := fakeClient.Create(ctx, &emptyNode)
+				if err != nil {
+					print(err.Error())
+				}
+				emptyNode.Annotations = annotations
+				err = fakeClient.Update(ctx, &emptyNode)
+				if err != nil {
+					print(err.Error())
+				}
+				deleteReq := admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{Name: test.name,
+					Operation: admissionv1.Delete,
+					UserInfo:  v1.UserInfo{Username: test.user},
+					Kind:      metav1.GroupVersionKind{Kind: "Node", Group: "core", Version: "v1"},
+					OldObject: runtime.RawExtension{Raw: nodeObj}}}
+				response := nv.Handle(ctx, deleteReq)
+				g.Expect(response.Allowed).Should(Equal(test.allowed))
+			}
+
+			// In case of cordon operation - create an empty node, and tries to cordon the node
+			// with the given user and reason and ensures the response is es expected.
+			if test.operation == "cordon" {
+				err := fakeClient.Create(ctx, &node)
+				if err != nil {
+					print(err.Error())
+				}
+				updateReq := admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{Name: test.name,
+					Operation: admissionv1.Update,
+					UserInfo:  v1.UserInfo{Username: test.user},
+					Kind:      metav1.GroupVersionKind{Kind: "Node", Group: "core", Version: "v1"},
+					OldObject: runtime.RawExtension{Raw: nodeObj},
+					Object:    runtime.RawExtension{Raw: cordonedNodeObj}}}
+				response := nv.Handle(ctx, updateReq)
+				g.Expect(response.Allowed).Should(Equal(test.allowed))
+
+				// In case of uncordon operation - create a cordoned node, and tries to uncordon the node
+				// with the given user and reason and ensures the response is es expected.
+				if test.operation == "uncordon" {
+					err := fakeClient.Create(ctx, &cordonedNode)
+					if err != nil {
+						print(err.Error())
+					}
+					UpdateReq := admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{Name: test.name,
+						Operation: admissionv1.Update,
+						UserInfo:  v1.UserInfo{Username: test.user},
+						Kind:      metav1.GroupVersionKind{Kind: "Node", Group: "core", Version: "v1"},
+						OldObject: runtime.RawExtension{Raw: cordonedNodeObj},
+						Object:    runtime.RawExtension{Raw: nodeObj}}}
+					response := nv.Handle(ctx, UpdateReq)
+					g.Expect(response.Allowed).Should(Equal(test.allowed))
+				}
+			}
+		})
+	}
+}
